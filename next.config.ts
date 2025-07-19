79d3383ce47daff5c8aeacbce5e45cd4f9e63d20
Yousif Alkhayyat <yalkhayy@gmail.com>
2025-07-19 16:39:06 -0500

    disable linter

--- next.config.ts
+++ next.config.ts
@@ -52,6 +52,13 @@ const nextConfig: NextConfig = {
     ],
     unoptimized: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
